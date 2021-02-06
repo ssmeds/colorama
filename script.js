5d6f99a67a7e14cbbaebd29a4adb030dd8d74dad
ssmeds <stina_smeds@hotmail.com>
2021-02-06 13:38:00 +0100

    created index, style and script files

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// Skapa en webbsida med ett input fält. I detta fält så skall vi kunna skriva en färg (på engleska, de färger som hateras av HTML/CSS) och när du klickar på spara så skall den skrivna färgen läggas som sidans bakgrundsfärg.
+
+// Tips: Här kan ni se vilka färger som finns tillgängliga
+// https://www.w3schools.com/colors/colors_names.asp
+
+
+
+// Bonus1. Skapa en array som testar om den lämnade färgen finns. Ge annars ett felmeddelande typ “Ogiltig färg”.
+// Visa även för besökaren innehållet från arrayn på något sätt.
+
+// Bonus2. Spara senaste sparade färgen i localStorage. Så när sidan laddas om så hämtas färgen därifrån om det finns någon!
